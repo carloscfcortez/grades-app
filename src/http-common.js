be758a8419443755e77cfecad9d6c79758695d9a
Carlos Cortez <carlos.cfcortez@gmail.com>
2020-09-06 00:18:46 -0400

    env variable

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const URL = process.env.REACT_APP_URL_BASE_API || 'http://localhost:5000/';
+
+
+const URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/';
 //Define a URL base da origem para consumo do servico
 export default axios.create({
   baseURL: URL,//'http://localhost:5000/', //process.env.URL_BASE_API,
