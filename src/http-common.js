f451f32d08cfa9ceb04d762fe2ecf67e73825f69
Carlos Cortez <carlos.cfcortez@gmail.com>
2020-09-06 00:43:30 -0400

    fix build

--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+console.log((process.env.REACT_APP_API_URL || 'http://localhost:5000/'));
 const URL = (process.env.REACT_APP_API_URL || 'http://localhost:5000/');
 //Define a URL base da origem para consumo do servico
 export default axios.create({
