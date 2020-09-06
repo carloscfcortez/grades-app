956fb1d4779ced8ffbd4e855a6c4572205bb4a74
Carlos Cortez <carlos.cfcortez@gmail.com>
2020-09-05 22:48:06 -0400

    fix params ids and set api url

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const URL = process.env.REACT_APP_URL_BASE_API || 'http://localhost:5000/';
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: URL,//'http://localhost:5000/', //process.env.URL_BASE_API,
   headers: {
     'Content-type': 'application/json',
   },
